8d906d779c0c2a91e7e4850394f204dbaca73745
David Vassallo <vassallo.davidm@gmail.com>
2023-02-26 19:34:29 +0000

    Add embed_many

--- embed.py
+++ embed.py
@@ -11,3 +11,14 @@ def embed_one(text):
         model='text-embedding-ada-002',
     )
     return np.array(response['data'][0]['embedding'], dtype=np.float32)
+
+
+def embed_many(texts):
+    """Embed multiple pieces of text."""
+    response = openai.Embedding.create(
+        input=texts,
+        model='text-embedding-ada-002',
+    )
+    # FIXME: Deal with the out of order case.
+    embeds = [d['embedding'] for d in response['data']]
+    return np.array(embeds, dtype=np.float32)
