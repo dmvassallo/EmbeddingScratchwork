3a730ad2ec6175999726f3f22dd470e62b2190b7
Eliah Kagan <degeneracypressure@gmail.com>
2023-03-23 17:27:55 +0000

    Monkeypatch manually in setUp and tearDown
    Because unittest.mock.patch relies on being able to delete the
    attribute.

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -3,7 +3,6 @@
 """Tests for the ``api_key`` property of the ``embed`` module."""
 
 import unittest
-import unittest.mock
 
 import openai
 
@@ -13,8 +12,21 @@ import embed
 class TestApiKey(unittest.TestCase):
     """Tests for ``embed.api_key``."""
 
-    @unittest.mock.patch('openai.api_key', 'sk-fake-redact-outer')
-    @unittest.mock.patch('embed.api_key', 'sk-fake-redact-inner')
+    def setUp(self):
+        """Pre-patch api_key attributes for purposes of log redaction."""
+        # This cannot be done straightforwardly with unittest.mock.patch
+        # because that expects to be able to delete attributes, and the
+        # embed.api_key property (deliberately) has no deleter.
+        self._real_key_openai = openai.api_key
+        self._real_key_embed = embed.api_key
+        openai.api_key = 'sk-fake-redact-outer'
+        embed.api_key = 'sk-fake-redact-inner'
+
+    def tearDown(self):
+        """Post-unpatch api_key attributes."""
+        embed.api_key = self._real_key_embed
+        openai.api_Key = self._real_key_openai
+
     def test_setting_sets_openai_api_key(self):
         """Setting ``embed.api_key`` sets both it and ``openai.api_key``."""
         pretend_key = 'sk-fake-setting-sets'
