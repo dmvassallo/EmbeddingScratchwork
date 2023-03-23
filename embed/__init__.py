7eae54df49710e78dda0a3085a943dd553306b92
Eliah Kagan <degeneracypressure@gmail.com>
2023-03-23 16:11:26 +0000

    Redesign the backing for embed.api_key
    This brings three improvements:
    
    1. It is easier to code for in the embed module (such as in the
       forthcoming functions that will use Requests instead of the
       OpenAI Python library), since code within embed now just
       accesses the api_key attribute of its nonpublic _keys submodule.
    
    2. It is set eagerly rather than lazily, to avoid unnecessarily
       diverging from the openai module's own behavior in when it reads
       the OPENAI_API_KEY environment variable. This is the only change
       that affects user-facing behavior (that is, behavior from the
       perspective of someone who is just using the embed module).
    
    3. It is simpler to initialize, because an initialization function
       in the nonpublic _keys submodule takes care of this when called.

--- embed/__init__.py
+++ embed/__init__.py
@@ -13,7 +13,6 @@ __all__ = [
 ]
 
 import operator
-import sys
 
 import backoff
 import numpy as np
@@ -22,11 +21,8 @@ import openai.embeddings_utils
 
 from . import _keys
 
-_self = sys.modules[__name__]
-"""This module, to access from its own code only. (For ``_self.api_key``.)"""
-
-# Give this module an api_key property that updates openai.api_key when set.
-_self.__class__ = _keys.KeyForwardingModule
+# Give this module an api_key property to be accessed from the outside.
+_keys.initialize(__name__)
 
 _backoff = backoff.on_exception(backoff.expo, openai.error.RateLimitError)
 """Backoff decorator for ``openai.Embedding.create``-based functions."""
--- embed/_keys.py
+++ embed/_keys.py
@@ -6,40 +6,51 @@ set, sets ``openai.api_key``. This is useful because code that consumes this
 project's ``embed`` module shouldn't have to use ``openai`` directly or know
 about ``openai.api_key``. (Setting ``openai.api_key`` to use ``requests``-based
 functions, which don't use ``openai``, would be especially unintuitive.)
+
+Code within the ``embed`` module itself may access ``_keys.api_key``.
 """
 
-# FIXME: Set embed.api_key eagerly rather than lazily, so its behavior doesn't
-#        needlessly diverge from that of openai.api_key.
-
-__all__ = ['KeyForwardingModule']
+__all__ = ['api_key', 'initialize']
 
 import os
+import sys
 import types
 
 import openai
 
+api_key = None
+"""OpenAI API key. This should only be accessed from ``__init__.py``."""
 
-class KeyForwardingModule(types.ModuleType):
-    """Module whose ``api_key`` property also sets ``openai.api_key``."""
+
+def initialize(module_or_name):
+    """
+    Give the module an ``api_key`` property and set it from the environment.
+
+    Setting the property sets ``openai.api_key`` (including this first time).
+    """
+    if isinstance(module_or_name, str):  # Because no match-case before 3.10.
+        module = sys.modules[module_or_name]
+    elif isinstance(module_or_name, types.ModuleType):
+        module = module_or_name
+    else:
+        raise TypeError(f'module_or_name is {type(module_or_name).__name__!r}')
+
+    # Give the module an api_key property that updates openai.api_key when set.
+    module.__class__ = _ModuleWithApiKeyProperty
+
+    # Set the property from the environment.
+    module.api_key = os.getenv('OPENAI_API_KEY')
+
+
+class _ModuleWithApiKeyProperty(types.ModuleType):
+    """A module whose ``api_key`` property also sets ``openai.api_key``."""
 
     @property
     def api_key(self):
         """OpenAI API key."""
-        try:
-            return self.__api_key
-        except AttributeError:
-            self.set_api_key_from_environment()
-            return self.__api_key
+        return api_key
 
     @api_key.setter
     def api_key(self, value):
-        self.__api_key = openai.api_key = value
-
-    def set_api_key_from_environment(self):
-        """
-        Set ``api_key`` from the ``OPENAI_API_KEY`` environment variable.
-
-        That happens automatically when the ``api_key`` property is first read.
-        But in multithreaded scenarios, this should be called ahead of time.
-        """
-        self.api_key = os.getenv('OPENAI_API_KEY')  # Calls the setter.
+        global api_key
+        api_key = openai.api_key = value
