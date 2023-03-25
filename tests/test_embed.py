b2b6e399d9924dfcd883080cb718f658963f210d
Eliah Kagan <degeneracypressure@gmail.com>
2023-03-25 17:14:25 -0400

    Assert that the backoff message is in the log
    This makes the unit test work properly *as* a unit test (assuming
    I've managed to iron the bugs out, which might not be the case).
    
    + Remove debugging cruft (the old print statement I was using).

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -5,6 +5,7 @@
 # pylint: disable=missing-function-docstring
 
 import os
+import re
 from typing import Any
 import threading
 import unittest
@@ -114,17 +115,28 @@ class TestBackoff(unittest.TestCase):
     """
     Tests that backoff works in the ``requests`` version.
 
-    This is hard to check for if one's OpenAI account is not subject to reduced
-    rate limits. Reduced limits are  only in the trial period and shortly
-    thereafter. But occasionally it may be valuable to test rate limiting
-    explicitly. So this sends a lot of requests to the OpenAI embeddings
-    endpoint in a short time.
+    This is hard to check for, if one's OpenAI account is not subject to
+    reduced rate limits. (Reduced rate limits are only in the trial period and
+    shortly thereafter.) But occasionally it may be valuable to test rate
+    limiting explicitly. So this sends a lot of requests to the OpenAI
+    embeddings endpoint in a short time.
     """
+
+    _LOG_MESSAGE_PATTERN = re.compile(
+        r'INFO:backoff:Backing off _post_request(\.\.\.) for [0-9.]+s '
+        r'\(embed\._RateLimitError\)',
+    )
 
     def setUp(self):
         """Reduce the risk of accidentally running this on CI."""
         if os.getenv('CI') is not None:
-            raise Exception(  # pylint: disable=broad-exception-raised
+            # pylint: disable=broad-exception-raised
+            #
+            # To signal a failure keeping the test from running at all, we
+            # raise a direct Exception instance, which code under test should
+            # never raise. (A more specific type would risk being misunderstood
+            # as a specific error related to the code under test.)
+            raise Exception(
                 "These tests shouldn't run via continuous integration.")
 
     def test_embed_one_req_backs_off(self):
@@ -137,12 +149,18 @@ class TestBackoff(unittest.TestCase):
             threading.Thread(target=run, args=(thread_index,))
             for thread_index in range(75)
         ]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
 
-        # FIXME: Assert that the expected "INFO" log message(s) were created.
+        with self.assertLogs() as log_context:
+            for thread in threads:
+                thread.start()
+            for thread in threads:
+                thread.join()
+
+        log_has_backoff_message = any(
+            self._LOG_MESSAGE_PATTERN.fullmatch(record.message)
+            for record in log_context.records
+        )
+        self.assertTrue(log_has_backoff_message)
 
 
 if __name__ == '__main__':
