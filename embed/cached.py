3fd4e6b15010a1640f862dd80926d093e44de933
David Vassallo <vassallo.davidm@gmail.com>
2023-03-26 00:02:34 +0000

    Add skeleton of caching decorator

--- embed/cached.py
+++ embed/cached.py
@@ -1,3 +1,23 @@
 """Versions of embedding functions that cache to disk."""
 
 import blake3
+import functools
+
+
+def cache_disk(func):
+
+    @functools.wraps(func)
+    def wrapper(text_or_texts):
+
+        # Compute hash
+
+        # Is hash already a key
+
+        # NO: Call the function
+
+        # NO: Store the key and embedding/s
+
+        # Return embedding/s
+        return ...
+
+    return wrapper
