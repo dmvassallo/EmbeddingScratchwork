c382a1e997fe6a1b82bbf357dd6c3b32bface228
David Vassallo <vassallo.davidm@gmail.com>
2023-03-29 04:04:17 +0000

    First draft decorator

--- embed/cached.py
+++ embed/cached.py
@@ -1,23 +1,39 @@
 """Versions of embedding functions that cache to disk."""
 
-import blake3
 import functools
+import json
+import pathlib
+
+import blake3
+import numpy as np
 
 
 def cache_disk(func):
 
     @functools.wraps(func)
     def wrapper(text_or_texts):
+        # Serialize text_or_texts
+        serialized = json.dumps(text_or_texts).encode()
 
         # Compute hash
+        hash1 = blake3.blake3(serialized).hexdigest()
 
-        # Is hash already a key
+        path = pathlib.Path('data', f'{hash1}.json')
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                parsed = json.load(file)
 
-        # NO: Call the function
+        except OSError:
+            # Get the embeddings
+            embeddings = func(text_or_texts)
 
-        # NO: Store the key and embedding/s
+            # Cache the embeddings
+            with open(path, 'x', encoding='utf-8') as file:
+                json.dump(embeddings.tolist())
 
-        # Return embedding/s
-        return ...
+        else:
+            embeddings = np.array(parsed, dtype=np.float32)
+
+        return embeddings
 
     return wrapper
