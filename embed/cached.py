b0a1f89b403b1596bb101e8b98ca3b54d06da359
David Vassallo <vassallo.davidm@gmail.com>
2023-03-29 04:25:12 +0000

    Various improvements

--- embed/cached.py
+++ embed/cached.py
@@ -7,30 +7,27 @@ import pathlib
 import blake3
 import numpy as np
 
+_DATA_DIR = pathlib.Path('data')
+"""Directory to cache embeddings."""
 
-def cache_disk(func):
 
+def disk_cache(func):
+    """Decorator to add disk caching to an embedding function."""
     @functools.wraps(func)
     def wrapper(text_or_texts):
-        # Serialize text_or_texts
         serialized = json.dumps(text_or_texts).encode()
+        basename = blake3.blake3(serialized).hexdigest()
+        path = _DATA_DIR / f'{basename}.json'
 
-        # Compute hash
-        hash1 = blake3.blake3(serialized).hexdigest()
-
-        path = pathlib.Path('data', f'{hash1}.json')
         try:
-            with open(path, 'r', encoding='utf-8') as file:
+            with open(path, mode='r', encoding='utf-8') as file:
                 parsed = json.load(file)
-
         except OSError:
-            # Get the embeddings
             embeddings = func(text_or_texts)
 
-            # Cache the embeddings
-            with open(path, 'x', encoding='utf-8') as file:
-                json.dump(embeddings.tolist())
-
+            embeddings_list = embeddings.tolist()
+            with open(path, mode='x', encoding='utf-8') as file:
+                json.dump(obj=embeddings_list, fp=file)
         else:
             embeddings = np.array(parsed, dtype=np.float32)
 
