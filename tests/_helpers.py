f8444de96549c92a8cee13ad3be60b274ad03370
Eliah Kagan <degeneracypressure@gmail.com>
2023-04-05 19:55:16 +0000

    Improve in-memory cache stats display + docstrings
    Among other improvements, this fixes a bug where the object
    representing the stats was always truthy (but evaluated as a
    boolean to decide whether to print a message). I had introduced
    that bug when I changed the stats from dict to SimpleNamespace. Now
    it is an attrs data class with with a custom __bool__.
    
    This also adds docstrings to Caller. (This is unrelated, in terms
    of function, but I was introducing a class for the stats, and it
    was convenient to write method docstrings for both at the same
    time.)
    
    Some of these changes facilitated reordering the top-level code in
    tests._helpers, which is also done here.

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -15,8 +15,9 @@ import os
 import pickle
 import re
 import sys
-import types
 import unittest.mock
+
+import attrs
 
 import embed
 
@@ -24,62 +25,6 @@ try:
     _cache_in_memory = functools.cache
 except AttributeError:  # No functools.cache before Python 3.9.
     _cache_in_memory = functools.lru_cache(maxsize=None)
-
-_in_memory_embedding_cache_stats = types.SimpleNamespace(misses=0, hits=0)
-"""Hits and misses of in-memory embeddings caches in tests. Not thread safe."""
-
-
-@atexit.register
-def _report_nontrivial_cache_statistics():
-    """Log global cache statistics, IF any caching has been performed."""
-    if _in_memory_embedding_cache_stats:
-        logging.info('In-memory cache stats: %r',
-                     _in_memory_embedding_cache_stats)
-
-
-def _logged_cache_in_memory_by(key, *, stats):
-    """
-    Similar to ``functools.cache``, but uses an arbitrary key selector.
-
-    Also, this logs, is only suitable for use in tests, and is not thread-safe.
-    """
-    def decorator(func):
-        cache = {}
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            cache_key = key(*args, **kwargs)
-            try:
-                value = cache[cache_key]
-            except KeyError:
-                stats.misses += 1
-                logging.debug('In-memory cache MISS #%d: %s',
-                              stats.misses, wrapper.__name__)
-                value = cache[cache_key] = func(*args, **kwargs)
-            else:
-                stats.hits += 1
-                logging.debug('In-memory cache HIT #%d: %s',
-                              stats.hits, wrapper.__name__)
-            return value
-
-        return wrapper
-
-    return decorator
-
-
-def _logged_cache_in_memory_for_testing(func):
-    """Wrap an embedding function and cache, so tests make fewer API calls."""
-    independent_cache = _logged_cache_in_memory_by(
-        key=pickle.dumps,
-        stats=_in_memory_embedding_cache_stats,
-    )
-    return independent_cache(func)
-
-
-def _identity_function(arg):
-    """Return the argument unchanged."""
-    return arg
-
 
 _truthy_config = re.compile(r'true|yes|1', re.IGNORECASE).fullmatch
 """Check if a configuration string should be considered to mean ``True``."""
@@ -127,6 +72,81 @@ def configure_logging():
     logging.basicConfig(level=getattr(logging, level))
 
 
+def _identity_function(arg):
+    """Return the argument unchanged."""
+    return arg
+
+
+@attrs.mutable
+class _CacheStats:
+    """Cache statistics (misses and hits)."""
+
+    misses = attrs.field(default=0)
+    """Number of times the cache was checked and an item was NOT found."""
+
+    hits = attrs.field(default=0)
+    """Number times the cache was checked and an an item was found."""
+
+    def __bool__(self):
+        """Whether any cache accesses (hits or misses) have occurred."""
+        return self.misses != 0 or self.hits != 0
+
+    def __str__(self):
+        """Representation for user interfaces. Doesn't show the type name."""
+        return f'misses={self.misses}, hits={self.hits}'
+
+
+def _logged_cache_in_memory_by(key, *, stats):
+    """
+    Similar to ``functools.cache``, but uses an arbitrary key selector.
+
+    Also, this logs, is only suitable for use in tests, and is not thread-safe.
+    """
+    def decorator(func):
+        cache = {}
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            cache_key = key(*args, **kwargs)
+            try:
+                value = cache[cache_key]
+            except KeyError:
+                stats.misses += 1
+                logging.debug('In-memory cache MISS #%d: %s',
+                              stats.misses, wrapper.__name__)
+                value = cache[cache_key] = func(*args, **kwargs)
+            else:
+                stats.hits += 1
+                logging.debug('In-memory cache HIT #%d: %s',
+                              stats.hits, wrapper.__name__)
+            return value
+
+        return wrapper
+
+    return decorator
+
+
+_in_memory_embedding_cache_stats = _CacheStats()
+"""Hits and misses of in-memory embeddings caches in tests. Not thread safe."""
+
+
+@atexit.register
+def _report_nontrivial_cache_statistics():
+    """Log global cache statistics, IF any caching has been performed."""
+    if _in_memory_embedding_cache_stats:
+        logging.info('In-memory cache stats: %s',
+                     _in_memory_embedding_cache_stats)
+
+
+def _logged_cache_in_memory_for_testing(func):
+    """Wrap an embedding function and cache, so tests make fewer API calls."""
+    independent_cache = _logged_cache_in_memory_by(
+        key=pickle.dumps,
+        stats=_in_memory_embedding_cache_stats,
+    )
+    return independent_cache(func)
+
+
 def _get_maybe_cache_embeddings_in_memory():
     """
     Get a decorator that may make test cases monkey-patch in in-memory caching.
@@ -151,13 +171,6 @@ def _get_maybe_cache_embeddings_in_memory():
     subclass of ``unittest.TestCase``, and the effect is the same as applying
     it to every ``test_*`` method in the class. (See ``unittest.mock.patch``.)
     """
-    # TODO: We have no stable interface, so drop this check after a short time.
-    if 'TESTS_CACHE_EMBEDDING_CALLS' in os.environ:
-        raise RuntimeError(
-            'The TESTS_CACHE_EMBEDDING_CALLS environment variable is no longer'
-            ' supported. Use TESTS_CACHE_EMBEDDING_CALLS_IN_MEMORY instead.',
-        )
-
     if not getenv_bool('TESTS_CACHE_EMBEDDING_CALLS_IN_MEMORY'):
         return _identity_function
 
@@ -175,13 +188,13 @@ maybe_cache_embeddings_in_memory = _get_maybe_cache_embeddings_in_memory()
 """Decorator that makes test case patch in in-memory caching, if enabled."""
 
 
-# FIXME: Add method docstrings.
 class Caller:
-    """Function proxy that supports monkey-patching."""
+    """Function proxy that supports monkey-patching module-level functions."""
 
     __slots__ = ('_module', '_name')
 
     def __init__(self, func):
+        """Make a ``Caller`` for some top-level function ``func``."""
         self._module = sys.modules[func.__module__]
         self._name = func.__name__
 
@@ -192,17 +205,22 @@ class Caller:
         raise RuntimeError(f'{func!r} is not {self!r}')
 
     def __repr__(self):
+        """Code-like representation for debugging. Possibly ``exec``-able."""
         return f'{type(self).__name__}({self._module}.{self._name})'
 
     def __str__(self):
+        """The ``str`` of the wrapped function (as it currently resolves)."""
         return str(self._resolve())
 
     def __call__(self, *args, **kwargs):
+        """Call the wrapped function (as it currently resolves)."""
         return self._resolve()(*args, **kwargs)
 
     @property
     def __name__(self):
+        """The name of the (original) wrapped function from its metadata."""
         return self._name
 
     def _resolve(self):
+        """Look up the function. If it's monkey-patched, this reflects that."""
         return getattr(self._module, self._name)
