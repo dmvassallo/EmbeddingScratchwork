19ec6ff85f4c0c90f47920b9eae72ff4b0336978
Eliah Kagan <degeneracypressure@gmail.com>
2023-04-10 19:21:27 +0000

    Remove subscribe/unsubscribe locking
    Locking would be needed in general but not the way we are using, or
    are ever likely to use, _audit in our tests.
    
    If the code in _audit is ever extracted into a *separate* project
    and put on PyPI, then the locking should probably be added back in.
    
    (It is a non-public helper module of our tests, so the absence of
    locking does not block future packaging/publishing of *this*
    project on PyPI.)

--- tests/_audit.py
+++ tests/_audit.py
@@ -4,12 +4,8 @@ __all__ = ['extract', 'skip_if_unavailable']
 
 import contextlib
 import sys
-import threading
 import unittest
 
-
-_lock = threading.Lock()
-"""Mutex protecting the table."""
 
 _table = None
 """Table mapping each event to its listeners, or None if not yet needed."""
@@ -18,10 +14,10 @@ _table = None
 def _hook(event, args):
     """Single audit hook used for all events and handlers."""
     try:
-        # For performance, don't lock. Subscripting a dict with str keys should
-        # be sufficiently protected by the GIL in CPython. This doesn't protect
-        # the table rows. But those are tuples that we always replace, rather
-        # than lists that we mutate, so we should observe consistent state.
+        # Subscripting a dict with str keys should be sufficiently protected by
+        # the GIL in CPython. This doesn't protect the table rows. But those
+        # are tuples that we always replace, rather than lists that we mutate,
+        # so we should observe consistent state.
         listeners = _table[event]
     except KeyError:
         return
@@ -34,13 +30,12 @@ def _subscribe(event, listener):
     """Attach a detachable listener to an event."""
     global _table
 
-    with _lock:
-        if _table is None:
-            _table = {}
-            sys.addaudithook(_hook)
+    if _table is None:
+        _table = {}
+        sys.addaudithook(_hook)
 
-        old_listeners = _table.get(event, ())
-        _table[event] = (*old_listeners, listener)
+    old_listeners = _table.get(event, ())
+    _table[event] = (*old_listeners, listener)
 
 
 def _fail_unsubscribe(event, listener):
@@ -50,26 +45,25 @@ def _fail_unsubscribe(event, listener):
 
 def _unsubscribe(event, listener):
     """Detach a listener that was attached to an event."""
-    with _lock:
-        if _table is None:
-            _fail_unsubscribe(event, listener)
+    if _table is None:
+        _fail_unsubscribe(event, listener)
 
-        try:
-            listeners = _table[event]
-        except KeyError:
-            _fail_unsubscribe(event, listener)
+    try:
+        listeners = _table[event]
+    except KeyError:
+        _fail_unsubscribe(event, listener)
 
-        # Work with the sequence in reverse to remove the most recent listener.
-        listeners_reversed = list(reversed(listeners))
-        try:
-            listeners_reversed.remove(listener)
-        except ValueError:
-            _fail_unsubscribe(event, listener)
+    # Work with the sequence in reverse to remove the most recent listener.
+    listeners_reversed = list(reversed(listeners))
+    try:
+        listeners_reversed.remove(listener)
+    except ValueError:
+        _fail_unsubscribe(event, listener)
 
-        if listeners_reversed:
-            _table[event] = tuple(reversed(listeners_reversed))
-        else:
-            del _table[event]
+    if listeners_reversed:
+        _table[event] = tuple(reversed(listeners_reversed))
+    else:
+        del _table[event]
 
 
 @contextlib.contextmanager
